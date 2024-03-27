729544dcd2d424debfe4d16710747da70e3493ab
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-03-28 00:35:52 +0600

    add: search section added

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:e_learing_app/utils/ThemeColors.dart';
 import 'package:flutter/material.dart';
 
@@ -13,50 +12,138 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: ThemeColors.bodyColor,
         body: SafeArea(
-      child: Column(children: [
-        Container(
-          padding: EdgeInsets.only(top: 20, left: 15, right: 15),
-          child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: Column(children: [
+            Container(
+              padding: EdgeInsets.only(top: 20, left: 15, right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'Hi Shakil',
-                    style: TextStyle(
-                        color: ThemeColors.textPrimaryColor,
-                        fontSize: 30,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w700),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hi Shakil',
+                        style: TextStyle(
+                            color: ThemeColors.textPrimaryColor,
+                            fontSize: 30,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700),
+                      ),
+                      Text(
+                        'Find your lessons today!',
+                        style: TextStyle(
+                            color: ThemeColors.textSecondaryColor,
+                            fontSize: 16,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
                   ),
-                  Text(
-                    'Find your lessons today!',
-                    style: TextStyle(
-                        color: ThemeColors.textSecondaryColor,
-                        fontSize: 16,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w500),
-                  ),
+                  Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 190, 190, 190).withOpacity(0.1),
+                            blurRadius: 15,
+                            offset: const Offset(0, 5),
+                          )
+                        ],
+
+                        ),
+                    child: const Image(
+                      image: AssetImage('assets/images/bell.png'),
+                      height: 30,
+                      width: 30,
+                    ),
+                  )
                 ],
               ),
-              Container(
-                height: 60,
-                width: 60,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(26, 146, 146, 146),
-                  borderRadius: BorderRadius.circular(16)
-                ),
-                child: const Image(image: AssetImage('assets/images/bell.png'),
-                height: 30,
-                width: 30,
-                ),
-              )
-            ],
-          ),
-        )
-      ]),
-    ));
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 15, right: 15),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 15),
+                      padding: EdgeInsets.only(
+                          top: 6, left: 15, right: 15, bottom: 6),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [
+                            BoxShadow(
+                            color: const Color.fromARGB(255, 190, 190, 190).withOpacity(0.1),
+                              blurRadius: 15,
+                              offset: const Offset(0, 5),
+                            )
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: SizedBox(
+                            child: Image(
+                                image: AssetImage('assets/images/search.png')),
+                          ),
+                          hintText: 'Search now...',
+                          hintStyle: TextStyle(
+                              color: ThemeColors.textSecondaryColor,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Filter'),
+                              content: Text('lorem ipsum dolor sit amet ...'),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Ok'))
+                              ],
+                            );
+                          });
+                    },
+                    child: Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: ThemeColors.primaryColor,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 190, 190, 190).withOpacity(0.1),
+                            blurRadius: 15,
+                            offset: const Offset(0, 5),
+                          )
+                        ],
+                      ),
+                      child: const Image(
+                        image: AssetImage('assets/images/filter.png'),
+                        height: 30,
+                        width: 30,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ]),
+        ));
   }
 }
--- lib/utils/ThemeColors.dart
+++ lib/utils/ThemeColors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ThemeColors {
-  static const Color primaryColor = Color(0xFF0D47A1);
+
+  static const Color bodyColor = Color(0xFFF9FBFF);
+  static const Color primaryColor = Color(0xFF3D8FEF);
   static const Color secondaryColor = Color(0xFF1976D2);
   static const Color textPrimaryColor = Color(0xFF2C2C2C);
   static const Color textSecondaryColor = Color(0xFF8C8C8C);
